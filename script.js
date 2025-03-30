ac1acf3c3d974f1bb5882b505ec9fb3d9670a68b
Raisul Tanna <raisulconnects@gmail.com>
2025-03-30 14:34:14 +0600

    final commit from Github.

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello From Raisul!");
+
+const asgaardSofa = document.querySelector(".asgaard");
+asgaardSofa.addEventListener("click", function () {
+  window.location.href = "product.html";
+});
